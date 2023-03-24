c0cfb9fe94835c644339ca05e33f4551909de890
Kolfering <benoithauquier@hotmail.com>
2023-03-24 14:29:41 +0100

    try custom port

--- vcpkg/install-arm-osx.sh
+++ vcpkg/install-arm-osx.sh
@@ -1,1 +1,1 @@
-`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --overlay-triplets=custom-triplets --triplet=arm64-osx --x-install-root=installed-arm64-osx --feature-flags="versions" install
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --overlay-ports=custom-ports --overlay-triplets=custom-triplets --triplet=arm64-osx --x-install-root=installed-arm64-osx --feature-flags="versions" install

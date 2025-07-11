c1cb3d97196d22cd258e6bdf2774f1d1746dcd11
Gregory Smith <wolfy@treellama.org>
2025-07-11 17:37:07 -0400

    Linux support for building Steam version of alephone, and also classic_marathon_launcher

--- Steam/steamshim_parent.cpp
+++ Steam/steamshim_parent.cpp
@@ -210,8 +210,10 @@ static bool launchChild(ProcessType *pid)
     auto app = findExe(boost::regex("Classic Marathon.*\\.app"));
     auto macos = app / "Contents" / "MacOS";
     auto bin = boost::filesystem::directory_iterator(macos)->path().string();
-#else
+#elseif defined(_WIN32)
     auto bin = findExe(boost::regex("Classic Marathon.*"));
+#else
+	auto bin = findExe(boost::regex("alephone"));
 #endif
 
     GArgv[0] = strdup(bin.c_str());
